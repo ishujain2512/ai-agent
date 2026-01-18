ef4d2c271362e2b63fb666ccf2e2c16f93cb6898
ishujain2512 <ishujain2512@gmail.com>
2026-01-18 16:39:03 +0530

    added agent feedback loop

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, argparse
+import os, argparse, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -32,30 +32,49 @@ def generate_response(contents):
     return response
 
 
-parser = argparse.ArgumentParser(description="Chatbot to help with code")
-parser.add_argument("user_prompt", type=str, help="Ask me Something")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot to help with code")
+    parser.add_argument("user_prompt", type=str, help="Ask me Something")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    for _ in range(20):
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+        ]
 
-response = generate_response(messages)
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-if response.function_calls:
-    function_call_results = []
-    for function_call in response.function_calls:
-        function_call_result = call_function(function_call)
-        if not function_call_result.parts:
-            raise Exception("No parts in function call result")
-        if not function_call_result.parts[0].function_response:
-            raise Exception("Function Response Object is None")
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("Function Response is None")
-        function_call_results.append(function_call_result.parts[0])
+        response = generate_response(messages)
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
         if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-else:
-    print(response.text)
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            function_call_results = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call)
+                if not function_call_result.parts:
+                    raise Exception("No parts in function call result")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("Function Response Object is None")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("Function Response is None")
+                function_call_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+
+            messages.append(types.Content(role="user", parts=function_call_results))
+        else:
+            print(response.text)
+            return
+    print(
+        "Error: The Chatbot has run out of iterations and has not yet been able to produce a good enough response please try again."
+    )
+    sys.exit(1)
+
+
+main()
